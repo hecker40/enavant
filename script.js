a5e8f1a1f47427e88b91d16599c45615c0c7a450
hecker40 <ultimatecoder40@gmail.com>
2025-07-30 14:08:55 -0700

    Create typewriter and carousel

--- script.js
+++ script.js
@@ -16,4 +16,48 @@ $(window).on('scroll', function() {
   
   function nav() {
     document.getElementById("navbar").classList.toggle("nav-open");
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ["math", "reading", "physics", "coding", "writing", "English", "history", "algebra"]
+const typewriter = document.getElementById("typewriter");
+
+let sleepTime = 100;
+
+let curPhraseIndex = 0;
+
+const whileLoop = async() => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      typewriter.innerText = curWord.substring(0, i+1);
+
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      typewriter.innerText = curWord.substring(0, i-1);
+
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 2);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    }
+
+    else {
+      curPhraseIndex++;
+    }
+
   }
+}
+
+whileLoop();
